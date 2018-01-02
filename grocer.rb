f9264a43d597929303aa32ff7ea1b755e055b670
Vic Patel <vp932712@gmail.com>
2018-01-02 23:54:39 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,24 @@
-def consolidate_cart(cart)
-  # code here
+def consolidate_cart(item)
+  final = Hash.new 0
+  semiFinal = Hash.new 0
+  empty = Hash.new 0
+item.each do |hash|
+  hash.each do |food, description|
+   final[food] +=1
+
 end
+end
+item.each do |hash|
+  hash.each do |food, description|
+  final.each do |key, value|
+    empty[:count] = value
+    semiFinal[key] = description.merge(empty)
+  end
+end
+end
+semiFinal
+end
+
 
 def apply_coupons(cart, coupons)
   # code here
