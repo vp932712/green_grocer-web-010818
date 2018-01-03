26e393babe1c1f534ec08f2d9322ccd47ebcf89a
Vic Patel <vp932712@gmail.com>
2018-01-03 00:30:54 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -32,3 +32,50 @@ def apply_coupons(cart, coupon)
   end
   cart
 end
+
+def apply_clearance(cart)
+
+  discount = 0.20
+  cart.each do |item, details|
+    if cart[item][:clearance] == true
+      cart[item][:price] = (cart[item][:price]*discount).round(1)
+    end
+  end
+  cart
+end
+
+def checkout(cart: [], coupons: [])
+  
+  total = 0
+  cart = consolidate_cart(cart)
+  
+  if cart.length == 1
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    if cart_clearance.length > 1
+      cart_clearance.each do |item, details|
+        if details[:count] >=1
+          total += (details[:price]*details[:count])
+        end
+      end
+    else
+      cart_clearance.each do |item, details|
+        total += (details[:price]*details[:count])
+      end
+    end
+  else
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    cart_clearance.each do |item, details|
+      total += (details[:price]*details[:count])
+    end
+  end
+  
+
+  if total > 100
+    total = total*(0.90)
+  end
+  total
+
+
+end
