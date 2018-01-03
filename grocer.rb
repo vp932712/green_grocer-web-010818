edf83a11b72cacecfb05e176c3dbd027d4a5c4ac
Vic Patel <vp932712@gmail.com>
2018-01-03 00:29:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,3 +15,20 @@ end
 end
 final
 end
+
+def apply_coupons(cart, coupon)
+  coupon.each do |item|
+    name_of_item = item[:item]
+    if cart.has_key?(name_of_item) == true && cart[name_of_item][:count] >= item[:num]
+      cart[name_of_item][:count] = cart[name_of_item][:count] - item[:num]
+      new_item = name_of_item + (" W/COUPON")
+      puts cart.has_key?(new_item)
+      if cart.has_key?(new_item) == false
+        cart[new_item] = {:price => item[:cost], :clearance => cart[name_of_item][:clearance], :count => 1}
+      else
+        cart[new_item][:count] += 1
+      end
+    end
+  end
+  cart
+end
